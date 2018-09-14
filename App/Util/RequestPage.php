eca23ba44a9eb03d1ce41daa8b2cc4f04e685435
SatellaSoft <satellasoft@gmail.com>
2018-09-14 17:14:31 -0300

    Security layer
    In this version all pages are protected if the user has common permission, so if he tries to access via a protected page, it will not be successful.

--- App/Util/RequestPage.php
+++ App/Util/RequestPage.php
@@ -1,10 +1,12 @@
 <?php
 
 $pagina = filter_input(INPUT_GET, "p", FILTER_SANITIZE_STRING);
+$permissao = intval($_SESSION['permissao']);
+
 $arrayPaginas = array(
     "home" => "View/home.php", //Página inicial
     "ajuda" => "View/ajuda.php",
-//Usuários
+    //Usuários
     "gusuario" => "View/UsuarioView/GerenciarUsuario.php",
     //Categorias
     "gcategoria" => "View/CategoriaView/GerenciarCategoria.php",
@@ -19,19 +21,35 @@ $arrayPaginas = array(
     "visualizarmodulo" => "View/ModuloView/VisualizarModulo.php",
 );
 
+$arrayProtegidas = array(
+    "gusuario",
+    "gcategoria",
+    "gprojeto",
+    "gvisualizaprojeto",
+    "gpermissao"
+);
+
 if ($pagina) {
     $encontrou = false;
+    $pageLoad = "";
 
     foreach ($arrayPaginas as $page => $key) {
         if ($pagina == $page) {
             $encontrou = true;
-            require_once($key);
+            $pageLoad = $key;
         }
     }
 
     if (!$encontrou) {
-        require_once("View/error.php");
+        $pageLoad = "View/error.php";
+    } else {
+        foreach ($arrayProtegidas as $pageProtected) {
+            if ($pageProtected == $pagina && $permissao == 2) {
+                $pageLoad = "View/permission.php";
+            }
+        }
     }
+    require_once($pageLoad);
 } else {
     require_once("View/home.php");
 }
---
+++ View/permission.php
@@ -0,0 +1,5 @@
+<div class="error">
+    <h1>Sem permissão</h1>
+    <p>Você não tem permissão para acessar essa página.</p>
+    <a href="?p=home" class="btn btn-danger">Voltar para a página inicial</a>
+</div>
